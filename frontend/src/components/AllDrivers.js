ae6159e8a668d57ebfb5e61f32a20b18a30b9b67
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-11 16:40:20 +0530

    driver card updated

--- frontend/src/components/AllDrivers.js
+++ frontend/src/components/AllDrivers.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {Link} from "react-router-dom";
+import '../App.css';
 //import {Link, useParams} from "react-router-dom";
 import '../AddDriver.css';
 
@@ -28,31 +29,7 @@ export default function AllDrivers(){
         getDrivers();
 
     },[driver])
-
-
-        const deleteDriver = (id) =>{
-        // alert(id)
-        var answer = window.confirm("Are you sure")
-
-       if(answer){
-    axios.delete("http://localhost:8070/driver/delete/"+id).then((res)=>{
-            if(res?.status == 200){
-
-           
-
-                setTimeout(() => {
-
-                    Navigate('/all-table')
-
-                }, 3000);
-
-        }
-        })
-}    }
-    
-
-    
-
+  
 
      //search
 
@@ -76,51 +53,82 @@ export default function AllDrivers(){
         <input type="search" placeholder="Search for Drivers Here.." className="form-control" style={{marginBottom:20 }} onChange={(e) => setSearchInput(e.target.value)}></input>
         </div>
         <hr></hr>
-        <img className ="icon" style={{width:'1080px', height:'260px',alignItems:'center'}}src={process.env.PUBLIC_URL+"C:\fakepath\IMG_20190307_153701.jpg"}/> <br></br> 
-       <hr></hr>
+        
         {driver && filteredResults.map((d , index) => { 
           return (
+
+            <div className="card driverCards " key={index} style={{width: '16rem' }} >
+              <div className="card-body">
+                <div className="driver-card-image">
+                  <img src={d.d_image} alt="Driver Image" className="card-img" />
+                </div>
+                <div className="driver-card-details">
+                  
+                  <div class="row">
+                    <div class="col-8">
+                    <p className="card-title">{d.d_name}</p>
+                      <p className="card-text">{d.d_telephone}</p>
+                    </div>
+                    <div class="col-4">
+                      <img src={d.v_image} alt="vehicle" className="vehicle-img" />
+                    </div>
+
+                  </div>
+                  
+
+                </div>
+                
+                <center>
+                  
+                <Link to={"view/"+d._id}><button  className='btn btn-outline-success'>View more</button></Link>
+                
+                </center>
+            </div>
+            </div>
            
           
 
-            <tr key={index}> 
-            <th scope="row">{index+1}</th>
+            // <tr key={index}> 
+            // <th scope="row">{index+1}</th>
       
              
-             <tr>
-             <td> {d.d_image}</td>
-             </tr>
-             <tr>
-             <td><h3>{d.d_name}</h3></td>
-             </tr>
-             <tr>
-             <td><h5>{d.d_email}</h5></td>
-             </tr>
-             <tr>
-             <td><h5>{d.d_telephone}</h5></td>
-             </tr>
+            //  <tr>
+            //  <td> {d.d_image}</td>
+            //  </tr>
+            //  <tr>
+            //  <td><h3>{d.d_name}</h3></td>
+            //  </tr>
+            //  <tr>
+            //  <td><h5>{d.d_email}</h5></td>
+            //  </tr>
+            //  <tr>
+            //  <td><h5>{d.d_telephone}</h5></td>
+            //  </tr>
             
-             <tr>
-             <td><h5>{d.v_type}</h5></td>
-             </tr>
+            //  <tr>
+            //  <td><h5>{d.v_type}</h5></td>
+            //  </tr>
              
-             <tr>
-             <td><h5>{d.d_description}</h5></td>
-             </tr>
-             <hr></hr>
+            //  <tr>
+            //  <td><h5>{d.d_description}</h5></td>
+            //  </tr>
+            //  <hr></hr>
              
             
               
-             <div className="btn-bar">
-             <td> <Link to={"view/"+d._id}><button  className='btn btn-warning'>View more</button></Link> </td>
-                    </div>
-            </tr>
+            //  <div className="btn-bar">
+            //  <td> <Link to={"view/"+d._id}><button  className='btn btn-warning'>View more</button></Link> </td>
+            //         </div>
+            // </tr>
+
+            
            
         
         
         )})
           }
            <hr></hr>
+
         <div>
           <button id='btn1' style={{width:'180px', height:'40px',marginLeft:10, marginTop:10}} className="btn btn-primary">
             <a href="/add" target="_blank" style={{textDecoration:'none', color:'white'}}>Register as Driver</a>
