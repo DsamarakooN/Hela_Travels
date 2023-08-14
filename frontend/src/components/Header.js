09f95eb27bdaba4a2a93568793e1792f78a8168d
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-08-15 01:33:07 +0530

    header hover problem fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,10 +5,12 @@ import React from "react";
 import { Facebook, Instagram, Twitter, Pinterest } from 'react-bootstrap-icons';
 import "../App.css"
 import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 
 function Header() {
+
+  const location = useLocation();
 
     return(
         
@@ -22,34 +24,36 @@ function Header() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
           <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/' activeClassName='active-nav-link'>Home</NavLink>
-              </Nav.Link>
+             
+                <NavLink exact to='/' className={`${location.pathname === "/" ? '':'active-nav-link'}`}>Home</NavLink>
+             
+            </Nav.Item>
+
+            <Nav.Item className="nav-item">
+             
+                <NavLink exact to='/about' className={`${location.pathname === "/about" ? '':'active-nav-link'}`}>Packages</NavLink>
+             
+            </Nav.Item>
+            
+            <Nav.Item className="nav-item">
+             
+                <NavLink exact to='/destinations' className={`${location.pathname === "/destinations" ? '':'active-nav-link'}`}>Destinations</NavLink>
+             
+            </Nav.Item>
+            <Nav.Item className="nav-item">
+             
+                <NavLink exact to='/Product' className={`${location.pathname === "/Product" ? '':'active-nav-link'}`}>Hotels</NavLink>
+             
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/about' activeClassName='active-nav-link'>Packages</NavLink>
-              </Nav.Link>
+             
+                <NavLink to='/contact' className={`${location.pathname === "/contact" ? '':'active-nav-link'}`}>Guides</NavLink>
+             
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/destinations' activeClassName='active-nav-link'>Destinations</NavLink>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/Product' activeClassName='active-nav-link'>Hotels</NavLink>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/contact' activeClassName='active-nav-link'>Guides</NavLink>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <NavLink to='/all' activeClassName='active-nav-link'>Drivers</NavLink>
-              </Nav.Link>
+             
+                <NavLink exact to='/all' className={`${location.pathname === "/all" ? '':'active-nav-link'}`}>Drivers</NavLink>
+             
             </Nav.Item>
           </ul>
           <ul className="navbar-nav sm-icons">
