963fe4d76afda5e778d51a38967c07d9ec6aa78f
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-09-05 03:17:01 +0530

    driver routes updated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,23 +11,11 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-// const URL = process.env.MONGODB_URL;
-
-// mongoose.connect(URL,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-    
-// });
-
-// const connection = mongoose.connection;
-// connection.once("open", () =>{
-//     console.log("✅Mongodb Connection Success!");
-// })
 
 const connectDB = require("./config/db.js");
 connectDB();
 
-const driverRouter = require("./routes/driver.js");
+const driverRouter = require("./routes/driverRoutes.js");
 
 app.use("/driver",driverRouter);
 
