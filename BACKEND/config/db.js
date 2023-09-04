2687a1e927c971cd40e03d0dfbef44b3c9e25f9a
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-09-04 22:52:14 +0530

    db.js updated

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     try{
         const con = await mongoose.connect(process.env.MONGODB_URL)
 
-        console.log(`✅MongoDB Connected: ${con.connection.host}`.cyan.underline)
+        console.log(`✅ MongoDB Connected: ${con.connection.host}`.cyan.underline)
     }catch(error){
-        console.error(`❌Error: ${error.message}`.red.underline.bold)
+        console.error(`❌ Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
