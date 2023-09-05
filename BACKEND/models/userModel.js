55bb9953310dcf53511dc3c8cd2dd641cdfb31ba
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-09-05 06:08:44 +0530

    user model created

---
+++ BACKEND/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            default: 'user',
+            enum: ['user', 'business']
+        },
+        businessType: {
+            type: String
+        },
+  
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = User = mongoose.model('users', UserSchema)
