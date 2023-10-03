39a1850785312d207a08abf782db80add1a034c9
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-10-03 23:02:32 +0530

    header updated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,13 +7,9 @@ import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { logout, reset } from '../features/auth/authSlice'
 import { FaSignInAlt, FaUser, FaAlignJustify  } from 'react-icons/fa'
 
-
-
-
 function Header() {
 
   const location = useLocation();
-
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
@@ -36,84 +32,66 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <Nav.Item>
-
                 <NavLink to='/' className={`${location.pathname === "/" ? 'active-nav-link' : ''}`}>Home</NavLink>
-                
               </Nav.Item>
 
               <Nav.Item className="nav-item">
-
                 <NavLink to='/packages' className={`${location.pathname === "/packages" ? 'active-nav-link' : ''}`}>Packages</NavLink>
-
               </Nav.Item>
 
               <Nav.Item className="nav-item">
-
                 <NavLink to='/destinations' className={`${location.pathname === "/destinations" ? 'active-nav-link' : ''}`}>Destinations</NavLink>
-
               </Nav.Item>
+
               <Nav.Item className="nav-item">
-
                 <NavLink to='/hotels' className={`${location.pathname === "/hotels" ? 'active-nav-link' : ''}`}>Hotels</NavLink>
-
               </Nav.Item>
-              <Nav.Item>
 
+              <Nav.Item>
                 <NavLink to='/guides' className={`${location.pathname === "/guides" ? 'active-nav-link' : ''}`}>Guides</NavLink>
-
               </Nav.Item>
+
               <Nav.Item>
-
                 <NavLink to='/all' className={`${location.pathname.startsWith("/all") ? 'active-nav-link' : ''}`}>Drivers</NavLink>
-
               </Nav.Item>
             </ul>
 
             <ul ul className="navbar-nav mb-2 mb-lg-0">
               {user ? (
-                <li>
-                  <div class="dropdown">
-                    <button type="button" class="btn btn-outline-success dropdown-toggle" data-bs-toggle="dropdown">
-                      <FaUser />
-                      {user.role === 'business' && (
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                          Biz
-                          <span class="visually-hidden">unread messages</span>
-                        </span>
-                      )}
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li>Hello, <br></br> {user.name.split(' ')[0]}</li>
-                      <li><a class="dropdown-item" href="/profile">Profile</a></li>
-                      <li><a class="dropdown-item" href="/profile">Settings</a></li>
-                      <li><a class="dropdown-item" href="#" onClick={onLogout}>Logout</a></li>
-                    </ul>
-                  </div>
-
-
-
-                </li>
-              ) : (
-                <>
-                  <li>
-                    <Link to='/login'>
-                      <FaSignInAlt /> Login
-                    </Link>
-                  </li>
-                </>
-              )}
+                        <li>
+                          <div class="dropdown">
+                            <button type="button" class="btn btn-outline-success dropdown-toggle" data-bs-toggle="dropdown">
+                              <FaUser />
+                              {user.role === 'business' && (
+                                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                  Biz
+                                  <span class="visually-hidden">unread messages</span>
+                                </span>
+                              )}
+                            </button>
+                            <ul class="dropdown-menu">
+                              <li>Hello, <br></br> {user.name.split(' ')[0]}</li>
+                              <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                              <li><a class="dropdown-item" href="/profile">Settings</a></li>
+                              <li><a class="dropdown-item" href="#" onClick={onLogout}>Logout</a></li>
+                            </ul>
+                          </div>
+                        </li>
+                      ) : (
+                            <>
+                              <li>
+                                <Link to='/login'>
+                                  <FaSignInAlt /> Login
+                                </Link>
+                              </li>
+                            </>
+                          )}
             </ul>
-
           </div>
-
         </div>
-
       </nav>
       <hr style={{ color: "#25D366" }} ></hr>
     </div>
-
-
-
   )
 }
 
